c875a60ddf54249c84758c6a04b6711e2375879e
Venus Rodin <VenusViolet1234@gmail.com>
2023-04-02 21:24:59 -1000

    inserted the get me query

--- Develop/client/src/components/LoginForm.js
+++ Develop/client/src/components/LoginForm.js
@@ -1,9 +1,11 @@
-// see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useQuery } from '@apollo/client';
+import { GET_ME } from './queries';
 
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+import UserProfile from './UserProfile';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
@@ -47,8 +49,14 @@ const LoginForm = () => {
     });
   };
 
+  const { loading, error, data } = useQuery(GET_ME);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
   return (
     <>
+      <UserProfile />
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
         <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
           Something went wrong with your login credentials!
